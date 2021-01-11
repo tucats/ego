e85df64e6ed54cece22b1b9eb7d21e9c237e6283
Tom Cole <tomcole@mac.com>
2021-01-11 11:36:45 -0500

    Rename profile keys for consistency

--- defs/constants.go
+++ defs/constants.go
@@ -24,21 +24,21 @@ const (
 
 	// File system location used to locate services, lib,
 	// and test directories
-	EgoPathSetting = "ego-path"
+	EgoPathSetting = "ego.path"
 
 	// Do we normalize the case of all symbols to a common
 	// (lower) case string. If not true, symbol names are
 	// case-sensitive.
-	CaseNormalizedSetting = "case-normalized"
+	CaseNormalizedSetting = "ego.compiler.normalized"
 
 	// What is the output format that should be used by
 	// default for operations that could return either
 	// "text" or "json" output.
-	OutputFormatSetting = "output-format"
+	OutputFormatSetting = "ego.output-format"
 
 	// If true, the script language includes language
 	// exteions such as print, call, try/catch.
-	ExtensionsEnabledSetting = "extensions-enabled"
+	ExtensionsEnabledSetting = "ego.extensions"
 
 	/////////////////////////////////////////////////////
 	//                                                 //
@@ -48,23 +48,23 @@ const (
 
 	// Should an interactive session automatically import
 	// all the pre-defined packages?
-	AutoImportSetting = "auto-import"
+	AutoImportSetting = "ego.compiler.import"
 
 	// Should the interactive RUN mode exit when the user
 	// enters a blank line on the console?
-	ExitOnBlankSetting = "exit-on-blank"
+	ExitOnBlankSetting = "ego.console.exit-on-blank"
 
 	// Should the copyright message be omitted when in
 	// interactive prompt mode?
-	NoCopyrightSetting = "no-copyright"
+	NoCopyrightSetting = "ego.console.no-copyright"
 
 	// Should the interactive command input processor use
 	// readline?
-	UseReadline = "use-readline"
+	UseReadline = "ego.console.use-readline"
 
 	// Should the Ego program(s) be run with static (true)
 	// or dynamic (false) types?
-	StaticTypesSetting = "static-types"
+	StaticTypesSetting = "ego.compiler.static-types"
 
 	/////////////////////////////////////////////////////
 	//                                                 //
@@ -73,15 +73,15 @@ const (
 	/////////////////////////////////////////////////////
 
 	// The base URL of the Ego server providing application services
-	ApplicationServerSetting = "application-server"
+	ApplicationServerSetting = "ego.application.server"
 
 	// The base URL of the Ego server providing logon services
-	LogonServerSetting = "logon-server"
+	LogonServerSetting = "ego.logon.server"
 
 	// The last token created by a ego logon command, which
 	// is used by default for server admin commands as well
 	// as rest calls.
-	LogonTokenSetting = "logon-token"
+	LogonTokenSetting = "ego.logon.token"
 
 	/////////////////////////////////////////////////////
 	//                                                 //
@@ -92,24 +92,24 @@ const (
 	// The default user if no userdatabase has been initialized
 	// yet. This is a strong of the form "user:password", which
 	// is defined as the root user.
-	DefaultCredentialSetting = "default-credential"
+	DefaultCredentialSetting = "ego.server.default-credential"
 
 	// If present, this user is always assigned super-user (root)
 	// privileges regardless of the userdata settings. This can be
 	// used to access an encrypted user data file.
-	LogonSuperuserSetting = "logon-superuser"
+	LogonSuperuserSetting = "ego.server.superuser"
 
 	// The file system location where the user database is stored.
-	LogonUserdataSetting = "logon-userdata"
+	LogonUserdataSetting = "ego.server.userdata.file"
 
 	// The encryption key for the userdata file. If not present,
 	// the file is not encrypted and is readable json.
-	LogonUserdataKeySetting = "logon-userdata-key"
+	LogonUserdataKeySetting = "ego.server.userdata.key"
 
 	// The key string used to encrypt authentication tokens.
-	TokenKeySetting = "token-key"
+	TokenKeySetting = "ego.token.key"
 
 	// A string indicating the duration of a token before it is
 	// considered expired. Examples are "15m" or "24h"
-	TokenExpirationSetting = "token-expiration"
+	TokenExpirationSetting = "ego.token.expiration"
 )
