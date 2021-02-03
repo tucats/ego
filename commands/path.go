f1f50923e5f31f084788664cba3226e83f57ccdb
Tom Cole <tomcole@mac.com>
2021-02-03 09:42:19 -0500

    fix: ego path when no preference set

--- commands/path.go
+++ commands/path.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/tucats/ego/app-cli/cli"
 	"github.com/tucats/ego/app-cli/persistence"
@@ -11,9 +13,25 @@ import (
 
 // PathAction is the command handler for the ego PATH command
 func PathAction(c *cli.Context) error {
+
+	// If there is already an envrionment variable, use that. Else get the
+	// preference setting.
 	p := os.Getenv("EGO_PATH")
 	if p == "" {
 		p = persistence.Get(defs.EgoPathSetting)
+	}
+
+	// If it's not an environment variable or a preference, see if we can infer
+	// it from the location of the program that launched us.
+	if p == "" {
+		p = c.FindGlobal().Args[0]
+		p, _ = filepath.Abs(p)
+		if strings.HasSuffix(p, ".exe") {
+			p = p[:len(p)-4]
+		}
+		if strings.HasSuffix(p, "ego") {
+			p = p[:len(p)-3]
+		}
 	}
 	fmt.Println(p)
 
