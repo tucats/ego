91734c634655c4e8381e05deed80a61d53bb81e2
vagrant <vagrant@localhost.localdomain>
2021-04-10 00:53:56 +0000

    support for linux builds

---
+++ commands/exec_linux.go
@@ -0,0 +1,25 @@
+// build darwin linux
+package commands
+
+import (
+	"os"
+	"syscall"
+)
+
+// runExec forks a detached process with the given file handle as
+// the stdout and stderr files.
+func runExec(cmd string, args []string, logf *os.File) (int, error) {
+	var attr = syscall.ProcAttr{
+		Dir: ".",
+		Env: os.Environ(),
+		Files: []uintptr{
+			os.Stdin.Fd(),
+			logf.Fd(),
+			logf.Fd(),
+		},
+	}
+
+	pid, err := syscall.ForkExec(args[0], args, &attr)
+
+	return pid, err
+}
