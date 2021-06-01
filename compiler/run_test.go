1fd9af3b1581313691ff27f623caf180e9885265
Tom Cole <tomcole@mac.com>
2021-06-01 17:29:27 -0400

    added framework for arbitrary compiler tests
    The TestArbitraryFragment test will compile a string containing any
    code fragment, and once compiled and executed, evaluates the "result"
    variable. This lets us test individual statements or compiler/runtime
    features without having to write Ego integration tests for
    the simpler cases.

---
+++ compiler/run_test.go
@@ -0,0 +1,50 @@
+package compiler
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/tucats/ego/errors"
+	"github.com/tucats/ego/symbols"
+)
+
+func TestArbitraryCodeFragments(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want interface{}
+	}{
+		// TODO: Add test cases. The text contains the entire program snipped to
+		// run, which _must_ create a variable named "result" which is compared to
+		// the wanted value. The program can be of arbitrary complexity, and need
+		// not be a complete program or function.
+
+		{
+			name: "decimal integer assignment",
+			text: "result := 1",
+			want: 1,
+		},
+		{
+			name: "octal integer assignment",
+			text: "result := 0o10",
+			want: 8,
+		},
+		{
+			name: "hexadecimal integer assignment",
+			text: "result := 0x10",
+			want: 16,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := symbols.NewRootSymbolTable(tt.name)
+			if err := RunString(tt.name, s, tt.text); !errors.Nil(err) && err.Error() != errors.Stop.Error() {
+				t.Errorf("Unexpected error %v", err)
+			}
+			result, found := s.Get("result")
+			if !reflect.DeepEqual(result, tt.want) || !found {
+				t.Errorf("Unexpected result; got %v, want %v", result, tt.want)
+			}
+		})
+	}
+}
