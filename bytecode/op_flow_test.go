1b216d3c96f032dc1e1c3bb6428637b94ea8d283
Tom Cole <tomcole@mac.com>
2021-07-31 17:21:29 -0400

    add flow  bytecode unit tests

---
+++ bytecode/op_flow_test.go
@@ -0,0 +1,135 @@
+package bytecode
+
+import (
+	"testing"
+
+	"github.com/tucats/ego/errors"
+)
+
+func Test_stopByteCode(t *testing.T) {
+	ctx := &Context{running: true}
+
+	e := stopByteCode(ctx, nil)
+	if !e.Equal(errors.Stop) {
+		t.Errorf("stopByteCode unexpected error %v", e)
+	}
+
+	if ctx.running {
+		t.Errorf("stopByteCode did not turn off running flag")
+	}
+}
+
+func Test_panicByteCode(t *testing.T) {
+	ctx := &Context{
+		stack:        []interface{}{"test"},
+		stackPointer: 1,
+		running:      true,
+	}
+
+	e := panicByteCode(ctx, nil)
+	if !e.Equal(errors.ErrPanic) {
+		t.Errorf("panicByteCode unexpected error %v", e)
+	}
+
+	if e.GetContext() != "test" {
+		t.Errorf("panicByteCode wrong context %v", e)
+
+	}
+}
+
+func Test_branchFalseByteCode(t *testing.T) {
+	ctx := &Context{
+		stack:          make([]interface{}, 5),
+		stackPointer:   0,
+		running:        true,
+		programCounter: 1,
+		bc: &ByteCode{
+			instructions: make([]Instruction, 5),
+			emitPos:      5,
+		},
+	}
+
+	// Test if TOS is false
+	_ = ctx.stackPush(false)
+	e := branchFalseByteCode(ctx, 2)
+	if !errors.Nil(e) {
+		t.Errorf("branchFalseByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 2 {
+		t.Errorf("branchFalseByteCode wrong program counter %v", ctx.programCounter)
+	}
+
+	// Test if TOS is true
+	_ = ctx.stackPush(true)
+
+	e = branchFalseByteCode(ctx, 1)
+	if !errors.Nil(e) {
+		t.Errorf("branchFalseByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 2 {
+		t.Errorf("branchFalseByteCode wrong program counter %v", ctx.programCounter)
+	}
+
+	// Test if target is invalid
+	_ = ctx.stackPush(true)
+
+	e = branchTrueByteCode(ctx, 20)
+	if !e.Equal(errors.ErrInvalidBytecodeAddress) {
+		t.Errorf("branchFalseByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 2 {
+		t.Errorf("branchFalseByteCode wrong program counter %v", ctx.programCounter)
+	}
+}
+
+func Test_branchTrueByteCode(t *testing.T) {
+	ctx := &Context{
+		stack:          make([]interface{}, 5),
+		stackPointer:   0,
+		running:        true,
+		programCounter: 1,
+		bc: &ByteCode{
+			instructions: make([]Instruction, 5),
+			emitPos:      5,
+		},
+	}
+
+	// Test if TOS is false
+	_ = ctx.stackPush(false)
+	e := branchTrueByteCode(ctx, 2)
+	if !errors.Nil(e) {
+		t.Errorf("branchTrueByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 1 {
+		t.Errorf("branchTrueByteCode wrong program counter %v", ctx.programCounter)
+	}
+
+	// Test if TOS is true
+	_ = ctx.stackPush(true)
+
+	e = branchTrueByteCode(ctx, 2)
+	if !errors.Nil(e) {
+		t.Errorf("branchTrueByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 2 {
+		t.Errorf("branchTrueByteCode wrong program counter %v", ctx.programCounter)
+	}
+
+	// Test if target is invalid
+	_ = ctx.stackPush(true)
+
+	e = branchTrueByteCode(ctx, 20)
+	if !e.Equal(errors.ErrInvalidBytecodeAddress) {
+		t.Errorf("branchTrueByteCode unexpected error %v", e)
+	}
+
+	if ctx.programCounter != 2 {
+		t.Errorf("branchTrueByteCode wrong program counter %v", ctx.programCounter)
+	}
+
+}
