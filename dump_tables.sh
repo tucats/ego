9740dd31e10a049068a39579584c23b5fe335534
Tom Cole <tomcole@mac.com>
2022-01-23 10:54:17 -0500

    Start on backup processing for database tables

---
+++ dump_tables.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+#
+# Dump ego tables to a backup database. Suitable for running
+# as a CRON job.
+#
+
+BASE=~/pg_backups
+FILE=$BASE/ego-tables-dump=$(date +%d.%m.%y-%H.%M.%S)
+
+echo Writing backup to $FILE
+
+pg_dump ego_tables -f $FILE
