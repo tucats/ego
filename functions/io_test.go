7d58cf5af060214188f4efd176fd92461f58d810
Tom Cole <tomcole@mac.com>
2022-05-12 19:42:28 -0400

    Add unit test for path sandboxing

---
+++ functions/io_test.go
@@ -0,0 +1,45 @@
+package functions
+
+import (
+	"testing"
+
+	"github.com/tucats/ego/app-cli/settings"
+	"github.com/tucats/ego/defs"
+)
+
+func Test_sandboxName(t *testing.T) {
+	tests := []struct {
+		name    string
+		sandbox string
+		want    string
+	}{
+		{
+			name:    "/tmp/foo",
+			sandbox: "",
+			want:    "/tmp/foo",
+		},
+		{
+			name:    "/tmp/foo",
+			sandbox: "/tmp",
+			want:    "/tmp/foo",
+		},
+		{
+			name:    "/tmp/foo",
+			sandbox: "/bar",
+			want:    "/bar/tmp/foo",
+		},
+		{
+			name:    "tmp/foo",
+			sandbox: "/bar",
+			want:    "/bar/tmp/foo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			settings.Set(defs.SandboxPathSetting, tt.sandbox)
+			if got := sandboxName(tt.name); got != tt.want {
+				t.Errorf("sandboxName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
