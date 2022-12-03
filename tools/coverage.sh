e2e3e49774d1201d435ca59a2afd5909a11b199d
Tom Cole <tomcole@mac.com>
2022-12-03 14:42:09 -0500

    Minor improvements to coverage tool

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 #
 # Use the coverage tool to test the coverage and display it
-# as an HTML file. This assumes you have installed the 'cover'
-# tool in your dev environment, using 
-#
-#   go get golang.org/x/tools/cmd/cover
-#
+# as an HTML file. Make sure the 'cover' tool is installed:
 
+go get golang.org/x/tools/cmd/cover
+
+# Figure out if we are testing everything (the default) or
+# just one specific path set
 TESTPATH=$1
 
 if [[ "$1" == "" ]]; then
@@ -14,13 +14,14 @@ if [[ "$1" == "" ]]; then
 fi 
 echo "TEST PATH IS $TESTPATH"
 
+# Transient overage data is stored in /tmp directory
 FILE=/tmp/ego.coverage_data 
 
+# Run the requested tests, storing the profile data in the
+# temp location. Then launch the coverage reporting tool
+# using the local web browser.
 go test -coverprofile $FILE $TESTPATH
 go tool cover -html=$FILE
 
-#
 # Be a good citizen and clean up the coverage data file
-#
-
 rm $FILE
