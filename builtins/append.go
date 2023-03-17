1f400ee749bdc215e6c35e2e5c1099a0f36b2a22
Tom Cole <tomcole@mac.com>
2023-03-17 14:42:07 -0400

    Make append() type checking more nuanced
    1. If in dynamic mode, anything can be added to an array.
    
    2. If in relaxed mode, anything will be converted to the array base
       type before adding to the array.
    
    3. If in strict mode, the item being appended must be of the same
       base type as the array.

--- builtins/append.go
+++ builtins/append.go
@@ -2,6 +2,7 @@ package builtins
 
 import (
 	"github.com/tucats/ego/data"
+	"github.com/tucats/ego/defs"
 	"github.com/tucats/ego/errors"
 	"github.com/tucats/ego/symbols"
 )
@@ -13,7 +14,13 @@ func Append(s *symbols.SymbolTable, args data.List) (interface{}, error) {
 	result := make([]interface{}, 0)
 	kind := data.InterfaceType
 
+	typeChecking := defs.StrictTypeEnforcement
+	if v, found := s.Get(defs.TypeCheckingVariable); found {
+		typeChecking = data.Int(v)
+	}
+
 	for i, j := range args.Elements() {
+		// If this is the first item, and it's an array, process it specially.
 		if array, ok := j.(*data.Array); ok && i == 0 {
 			if !kind.IsInterface() {
 				if err := array.Validate(kind); err != nil {
@@ -21,20 +28,37 @@ func Append(s *symbols.SymbolTable, args data.List) (interface{}, error) {
 				}
 			}
 
+			// Flatten the base array (also  []interface{} ) and append to the
+			// result directly. If we didn't yet know the type, let's assume it's
+			// the type of the array items.
 			result = append(result, array.BaseArray()...)
 
 			if kind.IsInterface() {
 				kind = array.Type()
 			}
 		} else if array, ok := j.([]interface{}); ok && i == 0 {
+			// We also allow being passed a simple []interface{} in which case it is
+			// dumped into the array without further checking.
 			result = append(result, array...)
 		} else {
-			if !kind.IsInterface() && !data.TypeOf(j).IsType(kind) {
-				return nil, errors.ErrWrongArrayValueType.In("append")
+			// Verify that the item is compatible with the array type. We only do this if
+			// we are in relaxed or strict mode.
+			if typeChecking < defs.NoTypeEnforcement && !kind.IsInterface() && !data.TypeOf(j).IsType(kind) {
+				// Mismatched type. Do we complain, or fix it? If we can determine the state of
+				// the type checking system (stored in the symbol table) and it is an integer
+				// value greater than zero, then we are allowed to coerce the value to the
+				// appropriate type.
+				if typeChecking > defs.StrictTypeEnforcement {
+					j = data.Coerce(j, data.InstanceOfType(kind))
+				} else {
+					// Nope, we are in strict type checking mode, so complain and be done.
+					return nil, errors.ErrWrongArrayValueType.In("append")
+				}
 			}
 			result = append(result, j)
 		}
 	}
 
+	// Manufacture a new array from the result set, and return it as the result.
 	return data.NewArrayFromInterfaces(kind, result...), nil
 }
