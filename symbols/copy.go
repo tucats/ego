0bc728bb94f92a6f375f6cb84c7734d2fb2d4010
Tom Cole <tomcole@mac.com>
2023-11-22 10:24:41 -0500

    Ensure package data is always readonly when copied from another table
    This ensures that the shared data between the source and destination
    symbol tables is readonly. Also removed dead code for Copy() method,
    which did a shallow clone of a table. Also updated the swarm.ego
    example to dynamicaally acquire the server hostname instead of it
    being hard-coded into the example.

--- symbols/copy.go
+++ symbols/copy.go
@@ -1,42 +1,13 @@
 package symbols
 
 import (
-	"github.com/google/uuid"
 	"github.com/tucats/ego/data"
 )
 
-// Make a copy of the symbol table, retaining the same values
-// as before (in fact, the values are shared between the tables).
-// This is mostly used to create unique symbol and constant maps,
-// which are needed for shallow clones of a compiler.
-func (s *SymbolTable) Clone(withLock bool) *SymbolTable {
-	if withLock {
-		s.Lock()
-		defer s.Unlock()
-	}
-
-	t := SymbolTable{
-		Name:          s.Name,
-		forPackage:    s.forPackage,
-		parent:        s.parent,
-		symbols:       map[string]*SymbolAttribute{},
-		values:        s.values,
-		id:            uuid.New(),
-		size:          s.size,
-		scopeBoundary: true,
-		isRoot:        true,
-	}
-
-	for k, v := range s.symbols {
-		t.symbols[k] = v
-	}
-
-	return &t
-}
-
 // For a given source table, find all the packages in the table and put them
-// in the current table.
-func (s *SymbolTable) GetPackages(source *SymbolTable) (count int) {
+// in the current table. Note that the underlying package data is shared by
+// both tables, but cannot be modified by either.
+func (s *SymbolTable) CopyPackagesFromTable(source *SymbolTable) (count int) {
 	if source == nil {
 		return
 	}
@@ -45,6 +16,11 @@ func (s *SymbolTable) GetPackages(source *SymbolTable) (count int) {
 		v := source.GetValue(attributes.slot)
 		if p, ok := v.(*data.Package); ok {
 			s.SetAlways(k, p)
+
+			// Because we've made a copy of the package, we need to
+			// ensure that the copy is not modifiable.
+			s.symbols[k].Readonly = true
+			source.symbols[k].Readonly = true
 
 			count++
 		}
