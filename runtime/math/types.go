e54e0e50b0201537d092ba1e63cb5f8ca7375f2b
Tom Cole <tomcole@mac.com>
2024-12-03 14:04:15 -0500

    feat: add additional math package functions that are native to Go math package

--- runtime/math/types.go
+++ runtime/math/types.go
@@ -11,24 +11,93 @@ import (
 
 var initLock sync.Mutex
 
+func mathFunc(name string, fn func(float64) float64) data.Function {
+	return data.Function{
+		Declaration: &data.Declaration{
+			Name: name,
+			Parameters: []data.Parameter{
+				{
+					Name: "f",
+					Type: data.Float64Type,
+				},
+			},
+			Returns: []*data.Type{data.Float64Type},
+		},
+		Value:    fn,
+		IsNative: true,
+	}
+}
+
 func Initialize(s *symbols.SymbolTable) {
 	initLock.Lock()
 	defer initLock.Unlock()
 
 	if _, found := s.Root().Get("math"); !found {
 		newpkg := data.NewPackageFromMap("math", map[string]interface{}{
-			"Abs": data.Function{
+			"Abs":     mathFunc("Abs", math.Abs),
+			"Acos":    mathFunc("Acos", math.Acos),
+			"Acosh":   mathFunc("Acosh", math.Acosh),
+			"Asin":    mathFunc("Asin", math.Asin),
+			"Asinh":   mathFunc("Asinh", math.Asinh),
+			"Atan":    mathFunc("Atan", math.Atan),
+			"Atanh":   mathFunc("Atanh", math.Atanh),
+			"Cbrt":    mathFunc("Cbrt", math.Cbrt),
+			"Ceil":    mathFunc("Ceil", math.Ceil),
+			"Cos":     mathFunc("Cos", math.Cos),
+			"Cosh":    mathFunc("Cosh", math.Cosh),
+			"Erf":     mathFunc("Erf", math.Erf),
+			"Erfc":    mathFunc("Erfc", math.Erfc),
+			"Erfcinv": mathFunc("Erfcinv", math.Erfcinv),
+			"Erfinv":  mathFunc("Erfinv", math.Erfinv),
+			"Exp2":    mathFunc("Exp2", math.Exp2),
+			"Expm1":   mathFunc("Expm1", math.Expm1),
+			"Fabs":    mathFunc("Fabs", math.Abs),
+			"Floor":   mathFunc("Floor", math.Floor),
+			"Gamma":   mathFunc("Gamma", math.Gamma),
+			"Inf": data.Function{
 				Declaration: &data.Declaration{
-					Name: "Abs",
+					Name: "Inf",
+					Parameters: []data.Parameter{
+						{
+							Name: "sign",
+							Type: data.IntType,
+						},
+					},
+					Returns: []*data.Type{data.Float64Type},
+				},
+				Value:    math.Inf,
+				IsNative: true,
+			},
+			"IsInf": data.Function{
+				Declaration: &data.Declaration{
+					Name: "IsInf",
+					Parameters: []data.Parameter{
+						{
+							Name: "f",
+							Type: data.Float64Type,
+						},
+						{
+							Name: "sign",
+							Type: data.IntType,
+						},
+					},
+					Returns: []*data.Type{data.BoolType},
+				},
+				Value:    math.IsInf,
+				IsNative: true,
+			},
+			"IsNan": data.Function{
+				Declaration: &data.Declaration{
+					Name: "IsNan",
 					Parameters: []data.Parameter{
 						{
 							Name: "f",
 							Type: data.Float64Type,
 						},
 					},
-					Returns: []*data.Type{data.Float64Type},
+					Returns: []*data.Type{data.BoolType},
 				},
-				Value:    math.Abs,
+				Value:    math.IsNaN,
 				IsNative: true,
 			},
 			"Log": data.Function{
@@ -73,6 +142,32 @@ func Initialize(s *symbols.SymbolTable) {
 				},
 				Value: minimum,
 			},
+			"Mod": data.Function{
+				Declaration: &data.Declaration{
+					Name: "Mod",
+					Parameters: []data.Parameter{
+						{
+							Name: "dividend",
+							Type: data.Float64Type,
+						},
+						{
+							Name: "divisor",
+							Type: data.Float64Type,
+						},
+					},
+					Returns: []*data.Type{data.Float64Type},
+				},
+				Value:    math.Mod,
+				IsNative: true,
+			},
+			"NaN": data.Function{
+				Declaration: &data.Declaration{
+					Name:    "NaN",
+					Returns: []*data.Type{data.Float64Type},
+				},
+				Value:    math.NaN,
+				IsNative: true,
+			},
 			"Normalize": data.Function{
 				Declaration: &data.Declaration{
 					Name: "Max",
@@ -104,20 +199,29 @@ func Initialize(s *symbols.SymbolTable) {
 				},
 				Value: random,
 			},
-			"Sqrt": data.Function{
+			"Remainder": data.Function{
 				Declaration: &data.Declaration{
-					Name: "Sqrt",
+					Name: "Remainder",
 					Parameters: []data.Parameter{
 						{
-							Name: "f",
+							Name: "dividend",
+							Type: data.Float64Type,
+						},
+						{
+							Name: "divisor",
 							Type: data.Float64Type,
 						},
 					},
 					Returns: []*data.Type{data.Float64Type},
 				},
-				Value:    math.Sqrt,
+				Value:    math.Mod,
 				IsNative: true,
 			},
+			"Round":       mathFunc("Round", math.Round),
+			"RoundToEven": mathFunc("RoundToEven", math.RoundToEven),
+			"Sin":         mathFunc("Sin", math.Sin),
+			"Sinh":        mathFunc("Sinh", math.Sinh),
+			"Sqrt":        mathFunc("Sqrt", math.Sqrt),
 			"Sum": data.Function{
 				Declaration: &data.Declaration{
 					Name: "Sum",
@@ -132,6 +236,9 @@ func Initialize(s *symbols.SymbolTable) {
 				},
 				Value: sum,
 			},
+			"Tan":   mathFunc("Tan", math.Tan),
+			"Tanh":  mathFunc("Tanh", math.Tanh),
+			"Trunc": mathFunc("Trunc", math.Trunc),
 		})
 
 		pkg, _ := bytecode.GetPackage(newpkg.Name)
