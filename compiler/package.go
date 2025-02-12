0c1c28cf368414b8b78e13f75e829f399b150620
Tom Cole <tomcole@mac.com>
2025-02-12 11:50:06 -0500

    fix: ignore package main

--- compiler/package.go
+++ compiler/package.go
@@ -19,6 +19,24 @@ func (c *Compiler) compilePackage() error {
 
 	name = c.normalizeToken(name)
 
+	// Special case -- if this is the "main" package, we have no work to do.
+	if name.Spelling() == "main" {
+		return nil
+	}
+
+	// If we've already seen a package with this name, we can't redefine it.
+	// This is a common error in Go, but we'll check for it here to be more
+	// explicit about our error handling.
+	//
+	// Note: This check is done in the order of the packages being imported,
+	// so if we see a package we're not expecting, we know we're redefining
+	// something. This is a limitation of our current design. If we want to
+	// support multiple packages with the same name, we'll need to change the
+	// way we handle imports and redefinitions.
+	//
+	// TODO: Fix this limitation.
+	//
+	// Also, note that this check is not done during the actual compilation
 	if (c.activePackageName != "") && (c.activePackageName != name.Spelling()) {
 		return c.error(errors.ErrPackageRedefinition)
 	}
