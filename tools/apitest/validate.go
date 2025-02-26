6e110a66b95029580e1cfe1af54015b3933a575a
Tom Cole <tomcole@mac.com>
2025-02-25 21:38:31 -0500

    feat: add apitests for admin logger, cache, memory and for service factor

--- tools/apitest/validate.go
+++ tools/apitest/validate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -23,14 +24,126 @@ func validateText(test *Test) error {
 		}
 
 		switch t.Operator {
-		case "", "eq", ".eq.", "==", "=", "equals":
+		case "", "eq", ".eq.", "==", "=", "equals", "equal":
 			if value != expect {
 				return fmt.Errorf("%s, %s: expected '%s', got '%s'", test.Description, t.Name, t.Value, value)
 			}
 
-		case "!=", "ne", ".ne.", "<>":
+		case "!=", "ne", ".ne.", "<>", "not equal":
 			if value != expect {
 				return fmt.Errorf("%s, %s: expected '%s', got '%s'", test.Description, t.Name, t.Value, value)
+			}
+
+		case "<", "lt", ".lt.", "less than":
+			// See if this can be done as an integer comparison.
+			if iValue, err := strconv.Atoi(value); err == nil {
+				if iExpect, err := strconv.Atoi(expect); err == nil {
+					if iValue >= iExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be less than '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// See if this can be done as an float comparison.
+			if fValue, err := strconv.ParseFloat(value, 64); err == nil {
+				if fExpect, err := strconv.ParseFloat(expect, 64); err == nil {
+					if fValue >= fExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be less than '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// If not, just do string comparison.
+			if value >= expect {
+				return fmt.Errorf("%s, %s: expected '%s' to be less than '%s'", test.Description, t.Name, value, expect)
+			}
+
+		case "<=", "le", ".le.", "less than or equal":
+			// See if this can be done as an integer comparison.
+			if iValue, err := strconv.Atoi(value); err == nil {
+				if iExpect, err := strconv.Atoi(expect); err == nil {
+					if iValue > iExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be less than or equal to '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// See if this can be done as an float comparison.
+			if fValue, err := strconv.ParseFloat(value, 64); err == nil {
+				if fExpect, err := strconv.ParseFloat(expect, 64); err == nil {
+					if fValue > fExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be less than or equal to '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// If not, just do string comparison.
+			if value < expect {
+				return fmt.Errorf("%s, %s: expected '%s' to be less than or equal to '%s'", test.Description, t.Name, value, expect)
+			}
+
+		case ">=", "ge", ".ge.", "greater than or equal":
+			// See if this can be done as an integer comparison.
+			if iValue, err := strconv.Atoi(value); err == nil {
+				if iExpect, err := strconv.Atoi(expect); err == nil {
+					if iValue < iExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be greater than or equal to '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// See if this can be done as an float comparison.
+			if fValue, err := strconv.ParseFloat(value, 64); err == nil {
+				if fExpect, err := strconv.ParseFloat(expect, 64); err == nil {
+					if fValue < fExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be greater than or equal to '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// If not, just do string comparison.
+			if value < expect {
+				return fmt.Errorf("%s, %s: expected '%s' to be greater than or equal to '%s'", test.Description, t.Name, value, expect)
+			}
+
+		case ">", "gt", ".gt.", "greater than":
+			// See if this can be done as an integer comparison.
+			if iValue, err := strconv.Atoi(value); err == nil {
+				if iExpect, err := strconv.Atoi(expect); err == nil {
+					if iValue <= iExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be greater than '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// See if this can be done as an float comparison.
+			if fValue, err := strconv.ParseFloat(value, 64); err == nil {
+				if fExpect, err := strconv.ParseFloat(expect, 64); err == nil {
+					if fValue <= fExpect {
+						return fmt.Errorf("%s, %s: expected '%s' to be greater than '%s'", test.Description, t.Name, value, expect)
+					} else {
+						continue
+					}
+				}
+			}
+
+			// If not, just do string comparison.
+			if value <= expect {
+				return fmt.Errorf("%s, %s: expected '%s' to be greater than '%s'", test.Description, t.Name, value, expect)
 			}
 
 		case "contains", "has", ".contains,", ".has.", "includes":
