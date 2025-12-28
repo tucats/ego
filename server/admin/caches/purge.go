e24d8311ea2fa0bab73e2549b7a334a47064d0f7
Tom Cole <tomcole@mac.com>
2025-12-28 17:07:48 -0500

    feat: when purging server cache, also flush authorization memory caches

--- server/admin/caches/purge.go
+++ server/admin/caches/purge.go
@@ -3,6 +3,7 @@ package caches
 import (
 	"net/http"
 
+	"github.com/tucats/ego/caches"
 	"github.com/tucats/ego/server/assets"
 	"github.com/tucats/ego/server/server"
 	"github.com/tucats/ego/server/services"
@@ -11,6 +12,14 @@ import (
 // PurgeCacheHandler is the cache endpoint handler that purges all entries in the cache,
 // and then returns the (revised) cache status.
 func PurgeCacheHandler(session *server.Session, w http.ResponseWriter, r *http.Request) int {
+	// Fre eup the various caches used to support authentication and DSN handling.
+	caches.Purge(caches.AuthCache)
+	caches.Purge(caches.BlacklistCache)
+	caches.Purge(caches.DSNCache)
+	caches.Purge(caches.TokenCache)
+	caches.Purge(caches.UserCache)
+
+	// Release the entries in the user cache.
 	// Release the entries in the asset cache.
 	assets.FlushAssetCache()
 
