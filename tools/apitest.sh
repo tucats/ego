90e03caa3a09a50a355959d13ce64e4d2c841df9
Tom Cole <tomcole@mac.com>
2026-05-04 10:05:31 -0400

    fix: allow tools/apitest.sh to accept apitest options

--- tools/apitest.sh
+++ tools/apitest.sh
@@ -3,9 +3,14 @@
 # Runner for the apitests. The source lives in the tools directory, and
 # the tests are in a subdirectory below that. This exists so it can run
 # from anywhere.
+#
+# Parameters are passed thru to the underlying apitest code. So for example,
+# you can specify a different scheme to use in the urls for the tests with:
+#
+#    tools/apitest.sh -x SCHEME=http
 
 pushd $(ego path)/tools/apitest/
 go mod tidy
-go run . tests/
+go run . tests/ $1 $2 $3 $4 $5 $6
 popd
 
