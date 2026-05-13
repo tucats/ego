9d7f05f0ea961e535d3759d92675d1c194171433
Tom Cole <tomcole@mac.com>
2026-05-13 19:43:12 -0400

    fix: dsn Lookup() was panicing if DSN subsystem not initialized

--- dsns/lookup.go
+++ dsns/lookup.go
@@ -1,10 +1,17 @@
 package dsns
 
-import "github.com/tucats/ego/defs"
+import (
+	"github.com/tucats/ego/defs"
+	"github.com/tucats/ego/errors"
+)
 
 // Externally available function to lookup a DSN. Must provide the
 // username and dsn name strings.
 func Lookup(session int, user, name string) (*defs.DSN, error) {
+	if DSNService == nil {
+		return nil, errors.ErrNoSuchDSN.Context("DSN service not initialized")
+	}
+
 	dsn, err := DSNService.ReadDSN(session, user, name, true)
 	if err != nil {
 		return nil, err
