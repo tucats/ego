9a81f107301ae10a2eb58f875e78410fb66ed986
Tom Cole <tomcole@mac.com>
2026-08-24 13:55:13 -0400

    feat: add trivial redeploy tool

---
+++ tools/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Simply re-deploy tool.  Assumes ego is already installed
+# and running; you need to stop the server, pull an update,
+# build, and restart the server
+
+# Move into the Ego build area
+pushd $(ego path)
+
+# Pull any new code
+git pull
+
+# Build the current repo
+tools/build
+
+# stop the active server
+EGO_GRAMMAR=class ego server stop
+
+# refresh the binary
+cp ego ~/bin/
+
+# restart the server
+EGO_GRAMMAR=class ego server start -k -port=8080
+
+# back to where we came from
+popd
+
